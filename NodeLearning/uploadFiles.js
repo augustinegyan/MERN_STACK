9aef1119ab07c2e98364173bd7fe707ae89e27d6
augustinegyan <augustinehappysunshine369@gmail.com>
2023-06-07 21:18:29 +0000

    Never ever git add or push a node module

---
+++ NodeLearning/uploadFiles.js
@@ -0,0 +1,9 @@
+/*
+There is a very good module for working with file uploads, called "Formidable".
+
+The Formidable module can be downloaded and installed using NPM: 
+CC : w3schools
+
+[+] - npm install formidable
+*/
+const formidable = require ('formidable')
