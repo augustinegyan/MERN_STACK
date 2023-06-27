b828e6d2e2c7416deab8d6769a73904a893103fa
augustinegyan <augustinehappysunshine369@gmail.com>
2023-06-27 00:34:37 +0000

    Just lost counting of the Days .. urlBuilding today

---
+++ ExpressJS/urlBuilding.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/:id', function(req, res){
+   res.send('The id you specified is ' + req.params.id);
+});
+//some key value pairs with url in express
+app.get('/me/:fname/:id',(req ,res)=>{
+    res.send('first name : '+req.params.fname +'id :'+req.params.id);
+})
+app.listen(2000)
