d3f64bf96411b4424b1b536125c5e510a4c9a664
augustinegyan <augustinehappysunshine369@gmail.com>
2023-07-01 11:17:38 +0000

    PATH modules

---
+++ ExpressJS/pathModule.js
@@ -0,0 +1,9 @@
+const path = require ('path')
+
+console.log(path.basename("C:/Users/20fingers/Desktop/MERN_STACK/ExpressJS/pathModule.js"))
+
+console.log(path.dirname("C:/Users/20fingers/Desktop/MERN_STACK/ExpressJS/pathModule.js"))
+
+console.log(path.extname("C:/Users/20fingers/Desktop/MERN_STACK/ExpressJS/pathModule.js"))
+
+console.log(path.parse("C:/Users/20fingers/Desktop/MERN_STACK/ExpressJS/pathModule.js"))
