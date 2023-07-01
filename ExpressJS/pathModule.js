d240120248e213260c91742841be121a88b88224
augustinegyan <augustinehappysunshine369@gmail.com>
2023-07-01 11:48:21 +0000

    PATH modules

--- ExpressJS/pathModule.js
+++ ExpressJS/pathModule.js
@@ -1,9 +1,22 @@
 const path = require ('path')
 
-console.log(path.basename("C:/Users/20fingers/Desktop/MERN_STACK/ExpressJS/pathModule.js"))
+
+
+
+console.log(__dirname)
+console.log(__filename)
+
+console.log(path.parse(__filename))
+
+// 2 forward slash (/) makes it the root 
+console.log(path.join("folder1","folder2","folder3"))
+console.log(path.join("/folder1","//folder2","folder3"))
+
+/*console.log(path.basename("C:/Users/20fingers/Desktop/MERN_STACK/ExpressJS/pathModule.js"))
 
 console.log(path.dirname("C:/Users/20fingers/Desktop/MERN_STACK/ExpressJS/pathModule.js"))
 
 console.log(path.extname("C:/Users/20fingers/Desktop/MERN_STACK/ExpressJS/pathModule.js"))
 
 console.log(path.parse("C:/Users/20fingers/Desktop/MERN_STACK/ExpressJS/pathModule.js"))
+*/
